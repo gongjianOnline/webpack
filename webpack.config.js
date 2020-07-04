cfed15dcd3daa4b270f4fcc308ccc2b22a3e25c9
龚箭 <15031617367@163.com>
2020-07-04 16:17:41 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var path = require('path');
 module.exports = {
     mode:"development",
@@ -6,5 +8,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         // filename: 'mian.js'
         filename:'[name].[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack_top',
+            template: 'src/assets/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 }
