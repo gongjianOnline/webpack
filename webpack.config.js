7e79edc82e2209ec3ea8b555396098f506dede63
龚箭 <15031617367@163.com>
2020-07-04 16:27:08 +0800

    cssUpdata

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var path = require('path');
 module.exports = {
     mode:"development",
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',  
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         // filename: 'mian.js'
@@ -23,4 +27,5 @@ module.exports = {
             },
         ],
     }
+    
 }
