3e27927c0766655935b79708046da69667df396e
龚箭 <15031617367@163.com>
2020-07-05 11:09:34 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import "./x.css"
+import "./x.scss"
+import "./y.less"
+import "./z.styl"
+import png from "./assets/1.jpg"
+
+const div = document.getElementById("app")
+console.log(div)
+console.log(png)
+div.innerHTML = `
+    <img src="${png}">
+`;
+const button = document.createElement("button")
+button.innerText="懒加载"
+button.onclick=()=>{
+    const promise = import("./lazy.js")
+    promise.then((module)=>{
+        module.default()
+    },()=>{
+
+    })
+}
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,16 +1,46 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename:'[name].[contenthash].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack_top',
-            template: 'src/assets/index.html'
-        }),
-    ]
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack_top",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+          //   'sass-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
